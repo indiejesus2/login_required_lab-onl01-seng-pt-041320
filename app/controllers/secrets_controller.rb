52bac0ffbf8a038e03281a80d6c414471a1e384b
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-09-24 23:23:26 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,19 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+
+    def show
+        @name = session[:name]
+    end
+
+    private 
+
+    def require_login
+
+        if session.include? :name
+            render :show
+        else
+            redirect_to '/login'
+        end
+    end
+
+end
